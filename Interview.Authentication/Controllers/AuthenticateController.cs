3b7bbf1514d43d33f82f10f32db8bd4d80620aa4
Administrator-0\Admin <renukanaidu09@gmail.com>
2017-10-28 23:24:16 +0100

    modified the return type

--- Interview.Authentication/Controllers/AuthenticateController.cs
+++ Interview.Authentication/Controllers/AuthenticateController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace Interview.Authentication.Controllers
 {
@@ -11,13 +12,18 @@ namespace Interview.Authentication.Controllers
     {
 
         // GET api/authenticate?username="a@gmail.com"&password="123"
-        public string Get([FromUri] string username, [FromUri] string password)
+        public JsonResult<AuthResponse> Get([FromUri] string username, [FromUri] string password)
         {
             if (username == "cnu@gmail.com" && password == "cnu")
             {
-                return "SUCCESS";
+                return Json(new AuthResponse {Status = "SUCESS"});
             }
-            return "FAIL";
+            return Json(new AuthResponse { Status = "FAIL" });
         }
+    }
+
+    public class AuthResponse
+    {
+        public string Status { get; set; }
     }
 }
