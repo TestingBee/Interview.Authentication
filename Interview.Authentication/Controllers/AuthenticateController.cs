17cfdd95a4e11048bdfb08efccb3c31069db79ac
Administrator-0\Admin <renukanaidu09@gmail.com>
2017-10-28 22:24:21 +0100

    added new controller

---
+++ Interview.Authentication/Controllers/AuthenticateController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Interview.Authentication.Controllers
+{
+    public class AuthenticateController : ApiController
+    {
+
+        // GET api/authenticate?username="a@gmail.com"&password="123"
+        public string Get([FromUri] string username, [FromUri] string password)
+        {
+            if (username == "cnu@gmail.com" && password == "cnu")
+            {
+                return "SUCCESS";
+            }
+            return "FAIL";
+        }
+    }
+}
