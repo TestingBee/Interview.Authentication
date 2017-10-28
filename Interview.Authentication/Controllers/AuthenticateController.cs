69c957c0363bb52da71a48c2c49c7fe419212b26
Administrator-0\Admin <renukanaidu09@gmail.com>
2017-10-28 23:57:09 +0100

    added cors header

--- Interview.Authentication/Controllers/AuthenticateController.cs
+++ Interview.Authentication/Controllers/AuthenticateController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Results;
 
 namespace Interview.Authentication.Controllers
 {
+    [EnableCors(origins: "http://interview-1.apphb.com", headers: "*", methods: "*")]
     public class AuthenticateController : ApiController
     {
 
